eb613ef592b71d97842eb00157015e23b7a320a2
Billy <billy@local>
2026-07-13 08:39:55 +1000

    Revert "fix(shortestpath): prefer castable spells over W330 house"
    This reverts commit 69b1b5f774966bef3c9a965c8abd4525062efab1.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/poh/World330HostedHouseTransport.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/poh/World330HostedHouseTransport.java
@@ -10,9 +10,8 @@ import java.util.Set;
 
 public class World330HostedHouseTransport extends Transport {
     public World330HostedHouseTransport(WorldPoint destination) {
-        super(null, destination, "W330 hosted house: Teleport to house tablet",
-                TransportType.TELEPORTATION_ITEM, true, 19);
-        setItemIdRequirements(Set.of(Set.of(ItemID.POH_TABLET_TELEPORTTOHOUSE)));
+        super(destination, "W330 hosted house: Teleport to house tablet", TransportType.TELEPORTATION_ITEM, true, 19,
+                Set.of(Set.of(ItemID.POH_TABLET_TELEPORTTOHOUSE)));
     }
 
     public boolean execute() {
